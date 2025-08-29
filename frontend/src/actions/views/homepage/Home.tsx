ac7a3307607df327d88561f5b602d7f4844693d6
tsusdere <fishesandanime@gmail.com>
2025-08-28 20:09:24 -0500

    relative padding

--- frontend/src/actions/views/homepage/Home.tsx
+++ frontend/src/actions/views/homepage/Home.tsx
@@ -2,11 +2,11 @@ import BasicBreadcrumbs from '../../components/Breadcrumbs.tsx'
 const Home = () => {
   return (
     <div class="flex flex-col space-y-135">
-      <div class="bg-blue-500 ">
+      <div class="bg-blue-500 relative top-40">
         <BasicBreadcrumbs/>
       </div>
-      <div class="bg-black">02</div>
-      <div class="bg-black">03</div>
+      <div class="bg-black relative bottom-30">02</div>
+      <div class="bg-black relative bottom-30">03</div>
     </div>
   )
 };
